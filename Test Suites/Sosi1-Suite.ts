a59069b8147c2b9c78c3d1bd80c3559b7825b272
Prashant-jumpbyte <prashant.b@appgambit.com>
2019-09-06 17:38:52 +0530

    Suite file added

---
+++ Test Suites/Sosi1-Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sosi1-Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>539c590a-fdb6-4375-8217-0aeac32d5f5f</testSuiteGuid>
+   <testCaseLink>
+      <guid>4def1fba-5245-4175-b2d2-b6b436a32b66</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sosi1-Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
