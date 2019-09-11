4f93b1785072f8fe97eba144cbee149b2e934c50
Prashant-jumpbyte <prashant.b@appgambit.com>
2019-09-11 17:54:41 +0530

    googlesheet integration files

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -3,3 +3,13 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
+import java.lang.String
+
+
+def static "com.java.utilily.ReadSpreadsheet.getSpreadSheetRecords"(
+    	String spreadsheetId	
+     , 	String range	) {
+    (new com.java.utilily.ReadSpreadsheet()).getSpreadSheetRecords(
+        	spreadsheetId
+         , 	range)
+}
